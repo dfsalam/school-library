a4c842c3cbc5753f29a6f9a585505a1203732217
dfsalam <dfsalam@gmail.com>
2023-04-12 20:04:31 -0500

    Fix branch error

--- student.rb
+++ student.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
-class Student
+require './person'
+# frozen_string_literal: true
+
+# Class that represents a student
+class Student < Person
+  def initialize(age, classroom, name)
+    super(age, name, true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
 end
+
+student1 = Student.new(23, 'Static')
+puts(student1.play_hooky)
+puts(student1.age)
