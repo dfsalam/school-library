b33f24609622f91c5fd5c2a883d71d3b5e83c367
dfsalam <dfsalam@gmail.com>
2023-04-14 23:34:33 -0500

    Add date as instance variable in the rental class.

--- rental.rb
+++ rental.rb
@@ -5,11 +5,12 @@ require './book'
 
 # Class that represents a rental
 class Rental
-  attr_reader :person, :book
+  attr_reader :person, :book, :date
 
-  def initialize(person, book)
+  def initialize(person, book, date)
     @person = person
     @book = book
+    @date = date
     person.add_rental(self)
     book.add_rental(self)
   end
