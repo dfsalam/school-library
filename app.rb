3c6e061d202bf2ce19ef260e327fb2fc4333eecc
dfsalam <dfsalam@gmail.com>
2023-04-15 18:52:28 -0500

    Split the code to fix linters errors

--- app.rb
+++ app.rb
@@ -13,82 +13,25 @@ class App
   def run
     puts 'Welcome to the school library app!'
     loop do
-      puts <<~MENU
-        Please choose an option:
-        1 - List all books
-        2 - List all people
-        3 - Create a person
-        4 - Create a book
-        5 - Create a rental
-        6 - List all rentals for a given person id
-        7 - Exit
-      MENU
-      option = gets.chomp.to_i
-      case option
-      when 1
-        list_all_books(@books)
-      when 2
-        list_all_people(@people)
-      when 3
-        print 'Do you want to create a student(1) or a teacher(2)? [Input the number]:'
-        person_type = gets.chomp.to_i
-        case person_type
-        when 1
-          print 'Age: '
-          age = gets.chomp.to_i
-          print 'Name: '
-          name = gets.chomp
-          print 'Has parent permission? [Y/N]: '
-          permission = gets.chomp
-          if %w[y Y].include?(permission)
-            create_student(age, name, true, @people)
-          elsif %w[n N].include?(permission)
-            create_student(age, name, false, @people)
-          else
-            puts 'Invalid choice. Please try again.'
-          end
-        when 2
-          print 'Age: '
-          age = gets.chomp.to_i
-          print 'Name: '
-          name = gets.chomp
-          print 'Specialization: '
-          specialization = gets.chomp
-          # Create the teacher
-          create_teacher(age, name, specialization, @people)
-        else
-          puts 'Invalid choice. Please try again.'
-        end
-      when 4
-        print 'Title: '
-        title = gets.chomp
-        print 'Author: '
-        author = gets.chomp
-        create_book(title, author, @books)
-        puts 'Book created successfully'
-      when 5
-        puts 'Select a book from the following list by number'
-        @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-        book = gets.chomp.to_i
-        puts 'Select a person from the following list by number (not id)'
-        @people.each_with_index do |person, index|
-          puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-        end
-        person = gets.chomp.to_i
-        print 'Date: '
-        date = gets.chomp
-        create_rental(@people[person], @books[book], date, @rentals)
-        puts 'Rental created successfully'
-      when 6
-        puts 'ID of person:'
-        person_id = gets.chomp
-        list_rentals_by_id(person_id, @rentals)
-      when 7
+      option = inicial_menu
+      if option == 7
         puts 'Thank you for using this app!'
         break
-      else
-        puts 'Invalid choice. Please try again.'
       end
+      swith_case(option)
     end
+  end
+end
+
+def swith_case(option)
+  case option
+  when 1 then list_all_books(@books)
+  when 2 then list_all_people(@people)
+  when 3 then option3
+  when 4 then option4
+  when 5 then option5
+  when 6 then option6
+  else
+    puts 'Invalid choice. Please try again.'
   end
 end
