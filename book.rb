3c64694a101feb90d1d1f0c03a3f8db81fc2e0f3
dfsalam <dfsalam@gmail.com>
2023-04-14 23:36:53 -0500

    Set getter and setter for rentals in book class

--- book.rb
+++ book.rb
@@ -2,8 +2,7 @@
 
 # Class that represents a book
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
